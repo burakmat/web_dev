35e1b64e8a5955fbe3de2ffe00ab812f5e9bee43
Burak Mat <burakk.mat@gmail.com>
2023-03-20 22:50:54 +0300

    add mongoose method instances and statics

--- 01_mongoose/index.js
+++ 01_mongoose/index.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp');
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+.then(() => {
+	console.log("Connection open\n");
+}).catch(err => {
+	console.log("Connection close\n", err);
+})
 
 const movieSchema = new mongoose.Schema(
 	{
@@ -10,7 +15,10 @@ const movieSchema = new mongoose.Schema(
 	}
 );
 
-const Movie = mongoose.model("Movie", movieSchema);// automatically creates "movies" collection
+// movieSchema.methods.function;// to declare intsance method
+// movieSchema.statics.function;// to declare static schema function
+
+// const Movie = mongoose.model("Movie", movieSchema);// automatically creates "movies" collection
 // const sample = new Movie(
 // 	{
 // 		title: "Cars",
@@ -20,14 +28,14 @@ const Movie = mongoose.model("Movie", movieSchema);// automatically creates "mov
 // 	}
 // );
 
-Movie.insertMany([
-    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
-    { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
-    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
-    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
-    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
-])
-.then((data)=>{console.log("Inserted"); console.log(data)})
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+// ])
+// .then((data)=>{console.log("Inserted"); console.log(data)})
 
 // Movie.update({year: 2001}, {year: 2000})
 // Movie.findOne({score: {$gt: 8.5}}).then(data=>console.log(data))
